95ce3d4cf9d1aca3195cc9d3984e58e875606168
Arsen-Cholakyan <arsencholakyan70@gmail.com>
2023-10-01 02:44:28 +0300

    homework6

--- src/bookExamples/Chapter5P2.java
+++ src/bookExamples/Chapter5P2.java
@@ -22,6 +22,76 @@ public class Chapter5P2 {
             i1++;
         }
 
+        //գրքի օրինակ 2 break-ը ներդրված for-ի մեջ
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Проход " + i + ": ");
+            for (int j = 0; j < 100; j++) {
+                if (j  == 10){
+                    break;
+                }
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("цикл завершон");
+
+
+        //գրքի օրինակ breakleibl-ի 1-ին օրինակ
+        boolean t = true;
+        first:{
+            second:{
+            third:{
+                System.out.println("Предшествует оператру break");
+                if (t == true){
+                    break second;
+                }
+                System.out.println("этот оператор не будет випалняться");
+            }
+                System.out.println("этот оператор не будет випалняться");
+
+            }
+            System.out.println("этот оператор не будет випалняться");
+        }
+
+
+        //գրքի օրինակ breakleibl-ի 2-րդ օրինակ
+        outer:{
+            for (int i = 0; i < 3; i++) {
+                System.out.println("проход" + i + ": ");
+                for (int j = 0; j < 100; j++) {
+                    if (j == 10) {
+                        break outer;
+                    }
+                    System.out.print(j + " ");
+                }
+                System.out.println("это страка не будет виведится");
+            }
+        }
+        System.out.println("цикл завершон");
+
+
+        //գրքի continue-ի օրինակ
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println();
+        }
+
+        //գրքի continue-ի օրինակ breakLeibl-ների հետ
+
+        outer:  for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (j > i ){
+                        System.out.println();
+                        continue outer;
+                    }
+                    System.out.print(" " + (i * j));
+                }
+            }
+            System.out.println();
+
 
     }
 }
--- src/myExamples/chapter5P2/MyExamplesC5P2.java
+++ src/myExamples/chapter5P2/MyExamplesC5P2.java
@@ -4,11 +4,15 @@ public class MyExamplesC5P2 {
     public static void main(String[] args) {
 
         //for ech-ի օրինակ
+        //int-տիպով
         int[] ars ={43,54,34,21,45};
         for (int x : ars){
             System.out.println(x);
         }
 
+
+        //for ech-ի օրինակ
+        //int-տիպով
         char[]ars1 = {'4','e','w'};
         for (char x:ars1){
             System.out.println(x);
